4a08a7750024873b5bbb1bab3d2e886c78dab850
mae616 <50073576+mae616@users.noreply.github.com>
2022-05-12 15:30:16 +0900

    buildできるように:homepageの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
