8c998706ae3c8a8eecb6467ae14330fa725e443a
mae616 <50073576+mae616@users.noreply.github.com>
2022-05-13 21:44:18 +0900

    花の形を詳細化

--- src/classes/Flower.ts
+++ src/classes/Flower.ts
@@ -9,7 +9,13 @@ import {
   MeshNormalMaterial,
   MeshStandardMaterial,
   Group,
+  Vector2,
+  LatheGeometry,
+  DoubleSide,
+  MeshBasicMaterial,
 } from "three";
+
+import { createMultiMaterialObject } from "three/examples/jsm/utils/SceneUtils.js";
 
 export class Flower extends Object3D {
   // flower: Object3D;
@@ -62,10 +68,13 @@ class Petals extends Group {
       // 配置座標を計算
       const radian = (i / length) * Math.PI * 2;
       petal.position.set(
-        30 * Math.cos(radian), // X座標
+        0.001 * Math.cos(radian), // X座標
         100, // Y座標
-        30 * Math.sin(radian) // Z座標
+        0.001 * Math.sin(radian) // Z座標
       );
+
+      petal.rotation.y = -radian;
+      petal.rotation.z += 1 / length - Math.cos((1 / length) * Math.PI * 2); // x軸方向に回転
 
       // グループに追加する
       this.add(petal);
@@ -74,17 +83,61 @@ class Petals extends Group {
 }
 
 // 花びら
-class Petal extends Mesh {
+class Petal extends Group {
   // petal: Mesh;
   constructor() {
-    // ジオメトリを作成
-    const geometry = new SphereGeometry(30, 30, 30);
+    super();
 
-    // マテリアルを作成
-    const material = new MeshNormalMaterial();
+    var points = [];
+    var height = 5;
+    var count = 30;
+
+    for (var i = 0; i < count; i++) {
+      points.push(
+        new Vector2(
+          (Math.sin(i * 0.08) * 2 + Math.cos(i * 0.08) * 2) * 10,
+          i - count + count + i * 1.8
+        )
+      );
+    }
+
+    var spGroup = new Group();
+    spGroup.rotation.y = -Math.PI / 2;
+    var material = new MeshBasicMaterial({
+      color: 0xff0000,
+      transparent: false,
+    });
+    points.forEach(function (point) {
+      var spGeom = new SphereGeometry(0.2);
+      var spMesh = new Mesh(spGeom, material);
+      spMesh.position.set(point.x, point.y, 0);
+      spGroup.add(spMesh);
+    });
+    this.add(spGroup);
+
+    // add the points as a group to the scene
+
+    var latheGeometry = new LatheGeometry(points, 50, 0, 1 * Math.PI);
+    var meshMaterial = new MeshNormalMaterial();
+    meshMaterial.side = DoubleSide;
+    var wireFrameMat = new MeshBasicMaterial();
+    wireFrameMat.wireframe = true;
+
+    var mesh = createMultiMaterialObject(latheGeometry, [
+      meshMaterial,
+      wireFrameMat,
+    ]);
+
+    this.add(mesh);
+
+    // // ジオメトリを作成
+    // const geometry = new SphereGeometry(30, 30, 30);
+
+    // // マテリアルを作成
+    // const material = new MeshNormalMaterial();
 
     // 継承元のコンストラクターを実行
-    super(geometry, material);
+    // super(geometry, material);
   }
 }
 
